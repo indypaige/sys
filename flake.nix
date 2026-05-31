6a4e49822cd9ad177fb5500c1046345c094e62ff
indy <dork@indypaige.dev>
2026-05-31 13:38:45 -0500

    mk:initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+        build = host:
+          { extraSpecialArgs ? []
+          , wallpaper        ? []
+          , extraEnv         ? {}
+          , modules          ? []
+          , inputs
+          , output
+          , email
+          , home
+          , ssh
+          }@builder: let
+            env  = extraEnv / builder;
+            home = {
+              home-manager.extraSpecialArgs = extraSpecialArgs / {
+                inherit env;
+
+                inputs = map (x: x.packages.${system}.default) inputs;
+              };
+
+              home-manager.useUserPackages  = true;
+              home-manager.users.${host}    = home;
+              home-manager.useGlobalPkgs    = true;
+            };
+          in {
+            nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
+              inherit system;
+
+              modules = modules ++ [
+                home-manager.nixosModules.home-manager
+                home
+              ];
+            };
+          };
+      }); }
