ddf4dda38b2148f8e07184cbabd1b9a033a40f7c
indy <dork@indypaige.dev>
2026-05-31 13:56:07 -0500

    mk: just take in system

--- flake.nix
+++ flake.nix
@@ -2,43 +2,41 @@
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, home-manager, ... }:
-    flake-utils.lib.eachDefaultSystem (system: {
-        build = host:
-          { extraSpecialArgs ? {}
-          , wallpaper        ? []
-          , extraEnv         ? {}
-          , packages         ? []
-          , modules          ? []
-          , output
-          , email
-          , home
-          , ssh
-          }@builder: let
-            env  = extraEnv // builder;
-            hm   = {
-              home-manager.extraSpecialArgs = extraSpecialArgs // {
-                inherit env;
+  outputs = { nixpkgs, home-manager, ... }: {
+    mk = system: host:
+      { extraSpecialArgs ? {}
+      , wallpaper        ? []
+      , extraEnv         ? {}
+      , packages         ? []
+      , modules          ? []
+      , output
+      , email
+      , home
+      , ssh
+      }@builder: let
+        env  = extraEnv // builder;
+        hm   = {
+          home-manager.extraSpecialArgs = extraSpecialArgs // {
+            inherit env;
 
-                packages = map (x: x.packages.${system}.default) packages;
-              };
-
-              home-manager.useUserPackages  = true;
-              home-manager.users.${host}    = home;
-              home-manager.useGlobalPkgs    = true;
-            };
-          in {
-            nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
-              inherit system;
-
-              modules = modules ++ [
-                home-manager.nixosModules.home-manager
-                hm
-              ];
-            };
+            packages = map (x: x.packages.${system}.default) packages;
           };
-    });
+
+          home-manager.useUserPackages  = true;
+          home-manager.users.${host}    = home;
+          home-manager.useGlobalPkgs    = true;
+        };
+      in {
+        nixosConfigurations.${host} = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          modules = modules ++ [
+            home-manager.nixosModules.home-manager
+            hm
+          ];
+        };
+      };
+  };
 }
